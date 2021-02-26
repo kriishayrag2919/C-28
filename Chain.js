13da0666b08eff921a4193614f73d1a4271599fa
kriishayrag2919 <75147334+kriishayrag2919@users.noreply.github.com>
2021-02-26 17:27:53 -0600

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,20 @@
+class Slingshot{
+    constructor(pointA, pointB){
+        var options = {
+            pointA: pointA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    display(){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.bodyB.position;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+    
+}
